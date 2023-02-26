00a04780777aac59c43124e7afaf034975d72a3f
Anna <lianna2015new@yandex.ru>
2023-02-26 12:57:25 +1000

    added program to find an even/odd number

---
+++ Practik3/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Введите ЛЮБОЕ число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write( number + " Это чётное число ");
+}
+else
+{
+    Console.Write( number + " Это нечётное число ");
+}
