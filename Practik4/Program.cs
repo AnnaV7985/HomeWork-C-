3459795afa75f83a9ef4706786e9f8ca99d470dc
Anna <lianna2015new@yandex.ru>
2023-02-26 14:05:36 +1000

    added program to find an even number

---
+++ Practik4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+for (int i = 1; i <= N; i++)
+    if (i % 2 ==0)
+ Console.WriteLine("четные числа из введенного числа: " + i); 
+
+
